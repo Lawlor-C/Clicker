80d1093cc153ef7221a62425f1595e1cdd42fb02
Chris Lawlor <chris.d.lawlor@gmail.com>
2025-01-14 20:39:33 +0000

    added a save-data feature upon each click which persists if tab is closed or refreshed

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Clicker
-Clicker Project
+I've decided I want to build a basic ever-growing Clicker browser game, to keep track of various facets I've learned during the School of Code bootcamp.
+
+I aim to add different functionality at certain levels reached, building on previous points reached. There will also be an option to reset the counter to Zero and lose all progress, as well as the ability to save progress locally for later.
--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+let score = document.getElementById("clickCount");
+//Upon initial load / reset the score is Zero
 let clickCount = 0;
 
+// Load data from localStorage when the page loads
+function autoLoad() {
+    const savedData = localStorage.getItem("data");
+    if (savedData) {
+        clickCount = parseInt(savedData);
+        score.innerText = clickCount;
+    }
+}
+autoLoad();
+
+// Save data to localStorage
+function saveData() {
+    localStorage.setItem("data", clickCount);
+}
+
+// Update the click count and save the data
 document.getElementById('clickButton').addEventListener('click', () => {
     clickCount++;
-    document.getElementById('clickCount').innerText = clickCount;
+    score.innerText = clickCount;
+    saveData();
 });
